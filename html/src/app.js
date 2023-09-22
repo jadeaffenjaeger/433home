a9deebb14b6e394c7801e36969756ebbc683a880
Jadeaffenjaeger <jadeaffenjaeger@gmx.net>
2023-09-22 19:34:48 +0200

    Use npm for dependencies

---
+++ html/src/app.js
@@ -0,0 +1,8 @@
+import "./custom.scss"
+import '@fontsource/lexend-peta/400.css';
+import '@fontsource/lexend-peta/800.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+
+const bootstrap = require('bootstrap')
+const htmx = require('htmx.org')
